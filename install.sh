8ab0b107b5c5fd56be1fdc95800a41e2d4cb0921
e2ky <17253583+e2ky@users.noreply.github.com>
2025-12-30 13:35:08 +0300

    updated install.sh to official ffmpeg for raspberry pi

--- install.sh
+++ install.sh
@@ -25,17 +25,20 @@ apt-get update && apt-get -y upgrade && apt-get install pkg-config python3-venv 
 
 # install ffmpeg
 echo "Downloading and installing ffmpeg"
-wget -q https://johnvansickle.com/ffmpeg/releases/ffmpeg-release-amd64-static.tar.xz
-mkdir -p tmp
-tar -xf ffmpeg-release-amd64-static.tar.xz --strip-components 1 -C tmp
-cp -v tmp/{ffmpeg,ffprobe,qt-faststart} /usr/local/bin
-rm -rf tmp ffmpeg-release-amd64-static.tar.xz
+# 
+# wget -q https://johnvansickle.com/ffmpeg/releases/ffmpeg-release-amd64-static.tar.xz
+# mkdir -p tmp
+# tar -xf ffmpeg-release-amd64-static.tar.xz --strip-components 1 -C tmp
+# cp -v tmp/{ffmpeg,ffprobe,qt-faststart} /usr/local/bin
+# rm -rf tmp ffmpeg-release-amd64-static.tar.xz
+#
+apt install -y ffmpeg
 echo "ffmpeg installed to /usr/local/bin"
 
 read -p "Enter portal URL, or press enter for localhost : " FRONTEND_HOST
 read -p "Enter portal name, or press enter for 'MediaCMS : " PORTAL_NAME
 
-[ -z "$PORTAL_NAME" ] && PORTAL_NAME='MediaCMS'
+[ -z "$PORTAL_NAME" ] && PORTAL_NAME='SawaFlix'
 [ -z "$FRONTEND_HOST" ] && FRONTEND_HOST='localhost'
 
 echo 'Creating database to be used in MediaCMS'
